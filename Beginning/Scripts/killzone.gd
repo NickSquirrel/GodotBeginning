d4b0d424c106e77f890ffd63450c355697a58487
LIGHTNING <2311790787@qq.com>
2026-08-24 16:25:39 +0800

    玩家死亡效果优化

--- Beginning/Scripts/killzone.gd
+++ Beginning/Scripts/killzone.gd
@@ -3,7 +3,10 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("you died")
+	Engine.time_scale=0.5
 	timer.start()
-
+	body.get_node("CollisionShape2D").queue_free()
 func _on_timer_timeout() -> void:
+	Engine.time_scale=1
 	get_tree().reload_current_scene()
+	
