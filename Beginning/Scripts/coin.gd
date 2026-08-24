a6e430bf1b50a9bbf9ab1761c2f01b6df53a81c6
LIGHTNING <2311790787@qq.com>
2026-08-24 17:01:14 +0800

    添加场景文字显示

--- Beginning/Scripts/coin.gd
+++ Beginning/Scripts/coin.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
+@onready var game_manager: Node = %GameManager
+
 
 func _on_body_entered(body: Node2D) -> void:
-	print("add 1 coin")
+	game_manager.add_point()
 	queue_free()
 
 	
